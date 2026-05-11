7f004c1c8bf7f4fdd229347932cacdeb08cb1568
Salomon <salomon.zougmore@it-projet.com>
2026-05-11 21:12:53 +0000

    First commit for great work

---
+++ GesPro/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "GesPro",
+    "depends": ["base"],
+    "installable": True,
+    "application": True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+    ],
+}
